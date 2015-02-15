a1715efe589a1d5fec8a95efaae56020fbbc315e
il.ya <saldin.iliya@gmail.com>
2015-02-15 18:22:39 +0600

    Initial Commit

---
+++ ftg_testjob/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ftg_testjob
+//
+//  Created by il.ya sald.in on 2/15/15.
+//  Copyright (c) 2015 sald.in. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ftg_testjobTests/ftg_testjobTests.m
@@ -0,0 +1,40 @@
+//
+//  ftg_testjobTests.m
+//  ftg_testjobTests
+//
+//  Created by il.ya sald.in on 2/15/15.
+//  Copyright (c) 2015 sald.in. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ftg_testjobTests : XCTestCase
+
+@end
+
+@implementation ftg_testjobTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
